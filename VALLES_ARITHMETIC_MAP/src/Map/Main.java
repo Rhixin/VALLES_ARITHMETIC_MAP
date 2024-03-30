d4dd2c7b3b625ba9df201be54faf574bd8a8e60b
Zhazted Rhixin V. Valles <134621548+Rhixin@users.noreply.github.com>
2024-03-30 23:52:42 +0800

    Update Main.java

--- VALLES_ARITHMETIC_MAP/src/Map/Main.java
+++ VALLES_ARITHMETIC_MAP/src/Map/Main.java
@@ -12,7 +12,7 @@ public class Main {
         String value;
 
         System.out.println("Instructions:");
-        System.out.println("1 - Add a key-value pair. Provide a key (an Integer value) and a value (a String value).\n\tExample: 1 101 hamburger\n\tNote: Please enter single-word strings only.");
+        System.out.println("1 - Add a key-value pair. Provide a key (an Integer value) and a value (a String value).\n\tExample: 1 101 ZhaztedPogi\n");
         System.out.println("2 - Get the value for a specific key. Provide the key (an Integer value).\n\tExample: 2 101");
         System.out.println("3 - Remove the value for a specific key. Provide the key (an Integer value).\n\tExample: 3 101");
         System.out.println("4 - Display the contents of the data structure.");
